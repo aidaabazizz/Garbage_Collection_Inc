3c0bbd0471a4bcc60c4d701d49213b34b0a6245b
Suchir-code <sjai0039@student.monash.edu>
2026-05-05 14:13:58 +0800

    Add transaction item contracts
    - Introduce Sellable for items sold to the Supercomputer
    - Introduce Purchasable for items bought from the Supercomputer
    - Define transaction hooks for immediate item effects

---
+++ src/game/capabilities/Purchasable.java
@@ -0,0 +1,31 @@
+package game.capabilities;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.finance.Wallet;
+
+/**
+ * Contract for items that can be purchased from the Supercomputer.
+ *
+ * @author Suchir
+ * @version 1.0
+ */
+public interface Purchasable {
+
+    /**
+     * Gets the purchase price of the item.
+     *
+     * @return purchase price in credits
+     */
+    int getPurchasePrice();
+
+    /**
+     * Applies the effect that occurs immediately after purchasing the item.
+     *
+     * @param buyer the actor buying the item
+     * @param map the current game map
+     * @param wallet the buyer's wallet
+     * @return description of the purchase effect
+     */
+    String purchasedBy(Actor buyer, GameMap map, Wallet wallet);
+}
---
+++ src/game/capabilities/Sellable.java
@@ -0,0 +1,31 @@
+package game.capabilities;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.finance.Wallet;
+
+/**
+ * Contract for items that can be sold to the Supercomputer.
+ *
+ * @author Suchir
+ * @version 1.0
+ */
+public interface Sellable {
+
+    /**
+     * Gets the current selling price of the item.
+     *
+     * @return selling price in credits
+     */
+    int getSellPrice();
+
+    /**
+     * Applies the effect that occurs immediately after selling the item.
+     *
+     * @param seller the actor selling the item
+     * @param map the current game map
+     * @param wallet the seller's wallet
+     * @return description of the selling effect
+     */
+    String soldBy(Actor seller, GameMap map, Wallet wallet);
+}
