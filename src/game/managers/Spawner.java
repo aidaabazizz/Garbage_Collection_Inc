18b9bdcfcc74cc4c3651fa0778d75a8221d712c6
chathyaattanayake <chathya@gmail.com>
2026-05-06 16:53:43 +0800

    Add spawnParasite method to Spawner interface for REQ4 - Added spawnParasite(Location location) method to Spawner interface - Enables parasite creature spawning through the spawner system - This extends the spawner contract to support new parasite enemy type.

--- src/game/managers/Spawner.java
+++ src/game/managers/Spawner.java
@@ -7,6 +7,7 @@ import edu.monash.fit2099.engine.positions.Location;
  * Facilitates the Dependency Inversion Principle between flora and environmental reactions.
  *
  * @author Jewell Gomes
+ * @author Chathya Attanayake
  */
 public interface Spawner {
     /**
@@ -19,4 +20,8 @@ public interface Spawner {
      * @param location The map location for the spawn.
      */
     public void spawnUndead(Location location);
+
+
+    //req4
+    void spawnParasite(Location location);
 }
