308a0b15acb9a4f872df2c3013e4917221200679
chathyaattanayake <chathya@gmail.com>
2026-05-06 16:32:34 +0800

    Add Parasite enemy with HuntBehaviour and WanderBehaviour - Created Parasite class extending NonPlayerCharacter - Display character: 'x' representing parasite enemy on game map - Initial health: 30 HP using BasicInventory - Added HuntBehaviour (priority 1) for hunting/infecting workers - Added WanderBehaviour (priority 999) for default movement when no target present

---
+++ src/game/actors/Parasite.java
@@ -0,0 +1,18 @@
+package game.actors;
+
+import game.BasicInventory;
+import game.behaviours.HuntBehaviour;
+import game.behaviours.WanderBehaviour;
+
+public class Parasite  extends NonPlayerCharacter{
+    private static final int INITIAL_HEALTH = 30;
+    private static final int WANDER_PRIORITY = 999;
+    private static final int INFECT_PRIORITY = 1;
+
+    public Parasite(){
+        super("Parasite",'x',INITIAL_HEALTH, new BasicInventory());
+        this.behaviours.put(INFECT_PRIORITY, new HuntBehaviour());
+        this.behaviours.put(WANDER_PRIORITY, new WanderBehaviour());
+    }
+
+}
