e3f587a2ca96be349336ab65a2bf6d82e00dea93
vict18031803 <vtay0004@student.monash.edu>
2026-05-07 00:35:03 +0800

    created a generic TeleportAction class that handles moving actor and allow every teleportable ground or item to use it

---
+++ src/game/actions/TeleportAction.java
@@ -0,0 +1,13 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+
+public class TeleportAction extends Action {
+
+    private final TeleportStrategy strategy;
+
+
+}
