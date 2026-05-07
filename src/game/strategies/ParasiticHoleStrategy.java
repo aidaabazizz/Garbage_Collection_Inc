d0c7dcfff1ef05e9eb842589798cbd43bab17869
chathyaattanayake <chathya@gmail.com>
2026-05-08 02:02:32 +0800

    Add ParasiticHoleStrategy for infected hole spawning behaviour
    - Created ParasiticHoleStrategy implementing HoleSpawnStrategy
    - Randomly spawns either Undead or Parasite creature at hole location
    - Uses 50/50 probability for each creature type

---
+++ src/game/strategies/ParasiticHoleStrategy.java
@@ -0,0 +1,14 @@
+package game.strategies;
+import edu.monash.fit2099.engine.positions.Location;
+import game.managers.Spawner;
+
+import java.util.Random;
+
+//req4
+public class ParasiticHoleStrategy implements HoleSpawnStrategy {
+    @Override
+    public void spawn(Location location, Spawner spawner) {
+        if (new Random().nextBoolean()) spawner.spawnUndead(location);
+        else spawner.spawnParasite(location);
+    }
+}
