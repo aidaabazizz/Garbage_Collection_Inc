3331f8013f5d77f180eac8bca06d5f0e3522f8d9
chathyaattanayake <chathya@gmail.com>
2026-05-08 04:20:25 +0800

    Update ParasiticHoleStrategy to return boolean spawn status
    - Changed spawn() method to return boolean reflecting spawn success
    - Returns spawner.spawnUndead() or spawner.spawnParasite() result directly
    - Added Random field for better random number generation (replaces new Random() each call)

--- src/game/holestrategies/ParasiticHoleStrategy.java
+++ src/game/holestrategies/ParasiticHoleStrategy.java
@@ -6,9 +6,15 @@ import java.util.Random;
 
 //req4
 public class ParasiticHoleStrategy implements HoleSpawnStrategy {
+    private final Random random = new Random();
+
     @Override
-    public void spawn(Location location, Spawner spawner) {
-        if (new Random().nextBoolean()) spawner.spawnUndead(location);
-        else spawner.spawnParasite(location);
+    public boolean spawn(Location location, Spawner spawner) {
+        // UPDATED REQ4: Spawns Undead and Parasites
+        if (random.nextBoolean()) {
+            return spawner.spawnUndead(location);
+        } else {
+            return spawner.spawnParasite(location);
+        }
     }
 }
