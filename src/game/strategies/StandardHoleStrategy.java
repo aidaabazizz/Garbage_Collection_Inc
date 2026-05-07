b9585613b56e31709b69bcfa4a76f9c9b52cd4e1
chathyaattanayake <chathya@gmail.com>
2026-05-08 02:02:42 +0800

    Add StandardHoleStrategy for normal hole spawning behaviour
    - Created StandardHoleStrategy implementing HoleSpawnStrategy
    - Randomly spawns either Undead or Slime creature at hole location
    - Uses 50/50 probability for each creature type

---
+++ src/game/strategies/StandardHoleStrategy.java
@@ -0,0 +1,15 @@
+package game.strategies;
+import edu.monash.fit2099.engine.positions.Location;
+import game.managers.Spawner;
+
+import java.util.Random;
+
+//req4
+public class StandardHoleStrategy implements HoleSpawnStrategy {
+
+    @Override
+    public void spawn(Location location, Spawner spawner) {
+        if (new Random().nextBoolean()) spawner.spawnUndead(location);
+        else spawner.spawnSlime(location);
+    }
+}
