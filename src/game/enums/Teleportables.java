8d15fea0045b7a93f040a840b9b5e41b4151ff26
vict18031803 <vtay0004@student.monash.edu>
2026-05-08 00:18:04 +0800

    created an enum for different teleport devices to do type check

---
+++ src/game/enums/Teleportables.java
@@ -0,0 +1,11 @@
+package game.enums;
+
+/**
+ * Enums for different teleport devices type check.
+ *
+ * @author Victoria Tay Wen Xie
+ * @version 1.0
+ */
+public enum Teleportables {
+    MAGIC_CIRCLE;
+}
