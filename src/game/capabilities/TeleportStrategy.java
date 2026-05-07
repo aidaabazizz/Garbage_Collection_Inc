78199158930ed893e61b4e9dccd75838ba0ed0e4
vict18031803 <vtay0004@student.monash.edu>
2026-05-07 09:04:47 +0800

    added the Javadoc for the TeleportStrategu
    - better understanding of template that will be used by all the teleportable items or grounds

--- src/game/capabilities/TeleportStrategy.java
+++ src/game/capabilities/TeleportStrategy.java
@@ -3,10 +3,15 @@ package game.capabilities;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
-import game.actors.ContractedWorker;
 
-import java.util.List;
-
+/**
+ * This is a strategy interface for teleportation behaviours and it defines the core
+ * operations of different teleportation mechanism. It serves as the template for all the
+ * teleportable grounds or items.
+ *
+ * @author Victoria Tay Wen Xie
+ * @version 1.0
+ */
 public interface TeleportStrategy {
     Location getDestination(Actor actor, GameMap map);
     void applySideEffects(Actor actor, Location source, Location destination, GameMap map);
