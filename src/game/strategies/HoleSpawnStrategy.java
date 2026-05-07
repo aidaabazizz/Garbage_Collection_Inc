9c9e0c53a3b6757e9d28aa29556fafd99dddef3d
chathyaattanayake <chathya@gmail.com>
2026-05-08 02:01:56 +0800

    Add HoleSpawnStrategy interface for hole spawning behaviour
    - Created HoleSpawnStrategy interface for REQ4 hole spawning mechanics
    - Defines spawn(Location location, Spawner spawner) method
    - Enables different spawning strategies for different hole types
    - Supports dependency injection of Spawner implementation

---
+++ src/game/strategies/HoleSpawnStrategy.java
@@ -0,0 +1,9 @@
+package game.strategies;
+
+import edu.monash.fit2099.engine.positions.Location;
+import game.managers.Spawner;
+
+//req4
+public interface HoleSpawnStrategy {
+    void spawn(Location location, Spawner spawner);
+}
