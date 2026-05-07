327656815f0ad9cf586f68b8bcf7aaef9700cc4e
JewellGomes <cloudsontheway@gmail.com>
2026-05-08 03:57:45 +0800

    req3, refactor: ux: improve terminal readability for ContractedWorker turns
    - Added display.endLine() at the start of playTurn to provide visual
      separation between game rounds.
    - Improves clarity when displaying status effects, facility alerts,
      and inventory notifications.

--- src/game/actors/ContractedWorker.java
+++ src/game/actors/ContractedWorker.java
@@ -54,6 +54,7 @@ public class ContractedWorker extends  Actor {
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
 
+        display.endLine();
         // Check global facility state
         if (AlarmManager.getInstance().isActive()) {
             display.println("\u001B[31m" + "!!! RED ALERT: FACILITY LOCKED DOWN !!!" + "\u001B[0m");
