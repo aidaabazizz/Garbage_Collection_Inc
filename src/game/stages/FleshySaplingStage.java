ff0a9e8d8a42afad30087602f8d23e360abaae29
JewellGomes <cloudsontheway@gmail.com>
2026-05-09 07:37:04 +0800

    req3, refactor: remove the Display import from the stages files for mutated flora

--- src/game/stages/FleshySaplingStage.java
+++ src/game/stages/FleshySaplingStage.java
@@ -1,6 +1,5 @@
 package game.stages;
 
-import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.Location;
 import game.grounds.AbstractTree;
 
--- src/game/stages/WarperMatureStage.java
+++ src/game/stages/WarperMatureStage.java
@@ -1,7 +1,6 @@
 package game.stages;
 
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.Location;
 import game.actions.TeleportAction;
 import game.grounds.AbstractTree;
