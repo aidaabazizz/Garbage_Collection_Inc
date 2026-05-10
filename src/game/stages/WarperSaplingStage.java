b45a87942aae314ba350c119b3dda8c7d8e1ac11
JewellGomes <cloudsontheway@gmail.com>
2026-05-11 04:29:18 +0800

    req3, refactor: decouple flora stages and improve teleportation strategy injection - Removed unused AbstractTree parameter from TreeStage interface to improve encapsulation. - Refactored WarperSaplingStage to inject TreeWarpStrategy into WarperMatureStage. - Aligned flora maturation logic with Dependency Inversion Principle by passing strategies instead of concrete actions.

--- src/game/stages/WarperSaplingStage.java
+++ src/game/stages/WarperSaplingStage.java
@@ -22,17 +22,15 @@ public class WarperSaplingStage extends AbstractTreeStage {
      * to determine if the tree matures into the next stage.
      *
      * @param location The current location of the tree on the map.
-     * @param tree The AbstractTree object this stage belongs to.
      * @return A new WarperMatureStage if maturation occurs; otherwise, this instance.
      */
     @Override
-    public TreeStage execute(Location location, AbstractTree tree) {
+    public TreeStage execute(Location location) {
         if (incrementAgeAndCheckGrowth(location, GROWTH_THRESHOLD, GROWTH_CHANCE, "Warper Sapling")) {
             display.println(String.format(
                     "Warper Tree Sapling ('%s') at %s matures into a Warper Mature Tree ('W')!",
                     getDisplayChar(), location));
-            var action = new TeleportAction(new TreeWarpStrategy());
-            return new WarperMatureStage(action);
+            return new WarperMatureStage(new TreeWarpStrategy());
         }
         return this;
     }
