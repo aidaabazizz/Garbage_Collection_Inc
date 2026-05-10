0cdf50ace33a241d9fe499724a555996fe15bb2b
bida <aidabazizzz@gmail.com>
2026-05-10 18:22:22 +0800

    Added all new Elsa states and behaviours.

---
+++ src/game/capabilities/BlizzardDisorientationStatus.java
@@ -0,0 +1,37 @@
+package game.capabilities;
+
+import edu.monash.fit2099.engine.GameEntity;
+import edu.monash.fit2099.engine.capabilities.Status;
+import edu.monash.fit2099.engine.positions.Location;
+
+/**
+ * A status that causes player movement to be randomized.
+ *
+ * @author Aida
+ */
+public class BlizzardDisorientationStatus implements Status {
+    private int remainingTurns;
+
+    public BlizzardDisorientationStatus(int duration) {
+        this.remainingTurns = duration;
+    }
+
+    @Override
+    public void tickStatus(GameEntity entity, Location location) {
+        remainingTurns--;
+    }
+
+    @Override
+    public boolean isStatusActive() {
+        return remainingTurns > 0;
+    }
+
+    public int getRemainingTurns() {
+        return remainingTurns;
+    }
+
+    @Override
+    public String toString() {
+        return "Blizzard disorientation (" + remainingTurns + " turns left)";
+    }
+}
---
+++ src/game/capabilities/Freezable.java
@@ -0,0 +1,10 @@
+package game.capabilities;
+
+/**
+ * Interface for actors that can be frozen by Elsa.
+ *
+ * @author Aida
+ */
+public interface Freezable {
+    void freeze(int duration);
+}
