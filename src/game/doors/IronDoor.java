dc22fa008e5d15898577959496bc0eda25df7a33
vict18031803 <vtay0004@student.monash.edu>
2026-05-10 13:33:50 +0800

    added Javadoc to the Iron Door class so that the side effects,constructor and the display character in the map menu is explained

--- src/game/doors/IronDoor.java
+++ src/game/doors/IronDoor.java
@@ -9,14 +9,32 @@ import game.enums.AccessLevel;
 import game.grounds.Door;
 import game.grounds.Fire;
 
+/**
+ * Iron door requires level 2 or higher clearance.
+ * Unlocking iron door causes the mechanism to overheat, setting adjavent tiles on fire
+ * for 2 turns.
+ *
+ * @author Victoria Tay Wen Xie
+ * @version 1.0
+ */
 public class IronDoor extends Door {
+    /** Fire duration in game turns*/
     private static final int FIRE_DURATION = 2;
+
+    /** Radius for adjacent tiles */
     private static final int ADJACENT_TILES = 1;
 
+    /**
+     * Constructor for the Iron door with level 2 clearance
+     */
     public IronDoor() {
         super(AccessLevel.LEVEL_TWO);
     }
 
+    /**
+     * Returns 'N' when locked, '_' when unlocked.
+     * @return display character based on lock state
+     */
     @Override
     public char getDisplayChar() {
         if (isUnlocked()) {
@@ -26,6 +44,12 @@ public class IronDoor extends Door {
         }
     }
 
+    /**
+     * This will set adjacent tiles on fire for 2 game turns when the door is unlocked
+     * @param actor the actor performing the unlock action
+     * @param map the current game map containing the door
+     * @return description of the overheating effect after unlocking it
+     */
     @Override
     public String applyUnlockEffect(Actor actor, GameMap map) {
         // gets the current position of the worker who unlocked the door
