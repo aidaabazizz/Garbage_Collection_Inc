6ec437f29a90dbbd98608626783dac9696417207
chathyaattanayake <chathya@gmail.com>
2026-05-11 13:49:15 +0800

    Update StandardHole constructor to accept Spawner dependency
    - Added Spawner parameter to StandardHole constructor
    - Passes spawner to super class Hole constructor
    - Enables dependency injection for consistent spawner usage

--- src/game/grounds/StandardHole.java
+++ src/game/grounds/StandardHole.java
@@ -2,9 +2,10 @@
 package game.grounds;
 
 import game.holestrategies.StandardHoleStrategy;
+import game.managers.Spawner;
 
 public class StandardHole extends Hole {
-    public StandardHole() {
-        super(new StandardHoleStrategy()); // Spawns Undead/Slime
+    public StandardHole(Spawner spawner) {
+        super(new StandardHoleStrategy(), spawner); // Spawns Undead/Slime
     }
 }
