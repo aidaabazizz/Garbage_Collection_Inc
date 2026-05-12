2c4b91f56f911b3443564f4e59d7b799ddcbea10
chathyaattanayake <chathya@gmail.com>
2026-05-13 02:23:43 +0800

    - Updated spawnCommonScrap() to accept Spawner parameter for Cookies constructor

--- src/game/EclipseNebula.java
+++ src/game/EclipseNebula.java
@@ -90,7 +90,7 @@ public class EclipseNebula extends World {
 
         // 7. Setup players
         // Start them on Moon 99 so they pick up the starting card and use the Tube
-        setupContractedWorkers(overflowMap);
+        setupContractedWorkers(overflowMap, globalSpawner);
 
     }
 
