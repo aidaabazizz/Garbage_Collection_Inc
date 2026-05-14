7812f7e13af1e2bde90e15c34f29a1363e8f9a0c
bida <aidabazizzz@gmail.com>
2026-05-14 13:50:13 +0800

    fix(req5): slimes now properly consume workers when hypnotized by Elsa
    - Replace asCapability() status check with direct class comparison
    - Slimes correctly detect HypnotizedStatus and attack adjacent workers
    - Add ConsumePlayerAction with damage logic (>3 HP = spat out with 1 HP, ≤3 HP = consumed)

--- src/game/actions/ConsumePlayerAction.java
+++ src/game/actions/ConsumePlayerAction.java
@@ -2,17 +2,20 @@ package game.actions;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.ActorStatistics;
 import edu.monash.fit2099.engine.positions.GameMap;
 
 /**
- * Action for slimes to consume players when Elsa is singing.
+ * Action for hypnotized slimes to swallow adjacent workers.
+ * If the worker has more than 3 HP, they are spat out with 1 HP.
+ * If the worker has 3 HP or less, they are knocked out.
  *
  * @author Aida
+ * @version 1.0
  */
 public class ConsumePlayerAction extends Action {
     private final Actor target;
     private final String direction;
-    private static final int DAMAGE = 5;
 
     public ConsumePlayerAction(Actor target, String direction) {
         this.target = target;
@@ -21,17 +24,27 @@ public class ConsumePlayerAction extends Action {
 
     @Override
     public String execute(Actor actor, GameMap map) {
-        target.hurt(DAMAGE);
-        String result = actor + " consumes " + target + " at " + direction + " for " + DAMAGE + " damage!";
+        int currentHealth = target.getStatistic(ActorStatistics.HEALTH);
+
+        if (currentHealth > 3) {
+            target.hurt(currentHealth - 1);
+            return String.format("\u001B[35m %s swallows %s at %s and spits them out with 1 HP left! \u001B[0m",
+                    actor, target, direction);
+        }
+
+        target.hurt(currentHealth);
+        String result = String.format("\u001B[31m %s DEVOURS %s at %s! The worker has been consumed! \u001B[0m",
+                actor, target, direction);
 
         if (!target.isConscious()) {
-            result += "\n" + target + " has been consumed!";
+            result += "\n" + target.unconscious(actor, map);
         }
+
         return result;
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return actor + " consumes " + target + " at " + direction;
+        return actor + " swallows " + target + " at " + direction;
     }
 }
