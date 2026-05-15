6228b20609fbb29768749dedfd90807b0c7bc75e
bida <aidabazizzz@gmail.com>
2026-05-15 19:04:55 +0800

    FIX: only player consumption triggers Elsa's ICE_SPIKE state.

--- src/game/utils/ConsumableUseTracker.java
+++ src/game/utils/ConsumableUseTracker.java
@@ -1,21 +1,44 @@
 package game.utils;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import game.enums.Ability;
+
 /**
- * Tracks whether a consumable item was used so Elsa can react with ICE_SPIKE.
+ * Tracks whether the PLAYER (ContractedWorker) consumed a consumable item.
+ * Used by Elsa to react with ICE_SPIKE state.
+ * Only tracks player consumption - NPC consumption (slimes, chickens, etc.)
+ * does NOT trigger ICE_SPIKE.
  *
  * @author Aida
- * @version 1.0
+ * @version 2.0
  */
 public class ConsumableUseTracker {
-    private static boolean consumedThisRound = false;
 
-    public static void markConsumed() {
-        consumedThisRound = true;
+    private static boolean playerConsumedThisRound = false;
+
+    /**
+     * Marks that an actor has consumed a consumable item this round.
+     * Only triggers the tracker if the actor is a player (has WORKER ability).
+     * Uses hasAbility() - NO instanceof!
+     *
+     * @param actor The actor who consumed the item
+     */
+    public static void markConsumed(Actor actor) {
+        // Uses capability pattern - NO instanceof!
+        if (actor.hasAbility(Ability.WORKER)) {
+            playerConsumedThisRound = true;
+        }
     }
 
+    /**
+     * Checks if the player consumed a consumable this round.
+     * Returns true only once per consumption, then resets.
+     *
+     * @return true if player consumed this round, false otherwise
+     */
     public static boolean consumeFlag() {
-        if (consumedThisRound) {
-            consumedThisRound = false;
+        if (playerConsumedThisRound) {
+            playerConsumedThisRound = false;
             return true;
         }
         return false;
