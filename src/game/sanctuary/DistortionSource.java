b317b8d6e480d9ad0c068515268cbcc70085cf7c
chathyaattanayake <chathya@gmail.com>
2026-06-01 03:38:05 +0800

    Add stabilise method to DistortionSource interface
    - Added stabilise(Location location) method to DistortionSource
    - Returns String description of stabilisation effect
    - Enables permanent or temporary shutdown of distortion sources
    - Supports CorruptedSafeHouse stabilisation mechanics

---
+++ src/game/sanctuary/DistortionSource.java
@@ -0,0 +1,11 @@
+package game.sanctuary;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+
+public interface DistortionSource {
+    String releaseDistortion(Actor actor, GameMap map, Location location);
+
+    String stabilise(Location location);
+}
