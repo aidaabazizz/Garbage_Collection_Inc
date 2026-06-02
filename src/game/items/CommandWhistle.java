37fe448171c4a9149f52aaebf47c7754681ca2af
chathyaattanayake <chathya@gmail.com>
2026-06-02 22:01:17 +0800

    Enhance CommandWhistle with weight tracking and worker targeting logic
    - Added weight statistic (1 unit) for inventory management
    - Implemented SpatialSearch.findNearestWorkerWithinDistance() for target selection
    - Whistle affects nearest worker within 5 tiles, falling back to user if none found
    - MotivationStatus anchored to worker's location as pulse origin
    - Item removed from inventory after use (consumable)

--- src/game/items/CommandWhistle.java
+++ src/game/items/CommandWhistle.java
@@ -3,25 +3,23 @@ package game.items;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
-import edu.monash.fit2099.engine.positions.GameMap;
-import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.positions.*;
+import edu.monash.fit2099.engine.statistics.BaseStatistic;
 import game.actions.UseCommandWhistleAction;
 import game.capabilities.MotivationStatus;
+import game.enums.Ability;
+import game.enums.ItemStatistics;
 import game.sanctuary.SanctuaryTool;
+import game.utils.SpatialSearch;
 
+public class CommandWhistle extends Item implements SanctuaryTool {
 
-
-public class CommandWhistle extends Item implements SanctuaryTool{
     public CommandWhistle() {
         super("Command Whistle", 'f');
         this.makePortable();
+        this.addNewStatistic(ItemStatistics.WEIGHT, new BaseStatistic(1));
     }
 
-    /**
-     * Returns the activation action when this whistle is in the actor's inventory.
-     *
-     * @return the list of available actions
-     */
     @Override
     public ActionList allowableActions(Actor owner, GameMap map) {
         ActionList actions = new ActionList();
@@ -31,9 +29,20 @@ public class CommandWhistle extends Item implements SanctuaryTool{
 
     @Override
     public String activateSanctuaryEffect(Actor actor, GameMap map, Location location) {
-        return new UseCommandWhistleAction(this).execute(actor, map);
-    }
+        // 1. SCAN: Use the utility to find WHO gets motivated
+        Actor pulseOriginActor = SpatialSearch.findNearestWorkerWithinDistance(map, location, 5);
+        if (pulseOriginActor == null) pulseOriginActor = actor;
 
+        // 2. APPLY: Give that worker the status.
+        // We pass 'location' (the user's spot) as the center of the blast.
+        pulseOriginActor.addStatus(new MotivationStatus(location));
+
+        // 3. CLEANUP
+        actor.getInventory().remove(this);
+
+        return String.format("%s blows the whistle! A pulse of leadership radiates from %s!",
+                actor, pulseOriginActor);
+    }
 
 
 }
