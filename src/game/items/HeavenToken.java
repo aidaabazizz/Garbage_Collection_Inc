07f30ccd48e48a2ee9a0612ef767facdb4e65474
chathyaattanayake <chathya@gmail.com>
2026-06-02 22:03:03 +0800

    Enhance HeavenToken with weight, portability, and sanctuary field deployment
    - Added weight statistic (1 unit) for inventory management
    - Enabled makePortable() to allow pickup from ground
    - Implemented activateSanctuaryEffect() to deploy SanctuaryField at actor's location
    - Stores previous ground for restoration when field expires
    - Removes token from inventory on use
    - Added purple colour formatting for activation message

--- src/game/items/HeavenToken.java
+++ src/game/items/HeavenToken.java
@@ -4,13 +4,19 @@ import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.statistics.BaseStatistic;
 import game.actions.ActivateHeavenTokenAction;
+import game.effects.SanctuaryField;
+import game.enums.ItemStatistics;
 import game.sanctuary.SanctuaryTool;
 
 public class HeavenToken extends Item implements SanctuaryTool {
     public HeavenToken() {
         super("Heaven Token", 'ε');
+        this.makePortable(); // FIX: Allows you to pick it up
+        this.addNewStatistic(ItemStatistics.WEIGHT, new BaseStatistic(1));
     }
 
     /**
@@ -29,7 +35,15 @@ public class HeavenToken extends Item implements SanctuaryTool {
 
     @Override
     public String activateSanctuaryEffect(Actor actor, GameMap map, Location location) {
-        // Logic to anchor a SanctuaryField object to this location for 10 turns
-        return actor + " uses the Heaven Token! A holy field manifests.";
+        Ground previousGround = location.getGround();
+
+        // Replace the floor Bob is standing on with a Sanctuary Field
+        location.setGround(new SanctuaryField(10, previousGround));
+
+        // Remove from inventory
+        actor.getInventory().remove(this);
+
+        return "\u001B[35m" + actor + " activates the Heaven Token! A protective field manifests!\u001B[0m";
     }
+
 }
