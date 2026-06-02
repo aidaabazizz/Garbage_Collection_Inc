c67faff89f6fb63c9a5126c9d1e25b3ba4cbc50d
chathyaattanayake <chathya@gmail.com>
2026-06-02 22:13:29 +0800

    Refactor BlizzardDisorientationStatus to extend TimedStatus
    - Extends TimedStatus abstract class for reuse
    - Removes redundant tickStatus() and isStatusActive() implementations
    - Maintains custom toString() with remaining turns display

--- src/game/capabilities/BlizzardDisorientationStatus.java
+++ src/game/capabilities/BlizzardDisorientationStatus.java
@@ -4,31 +4,7 @@ import edu.monash.fit2099.engine.GameEntity;
 import edu.monash.fit2099.engine.capabilities.Status;
 import edu.monash.fit2099.engine.positions.Location;
 
-/**
- * A status that causes player movement to be randomized.
- * Implements DisorientedCapability for capability pattern.
- *
- * @author Aida
- */
-public class BlizzardDisorientationStatus implements Status{
-    private int remainingTurns;
-
-    public BlizzardDisorientationStatus(int duration) {
-        this.remainingTurns = duration;
-    }
-
-    @Override
-    public void tickStatus(GameEntity entity, Location location) {
-        remainingTurns--;
-    }
-
-    @Override
-    public boolean isStatusActive() {
-        return remainingTurns > 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Blizzard disorientation (" + remainingTurns + " turns left)";
-    }
+public class BlizzardDisorientationStatus extends TimedStatus {
+    public BlizzardDisorientationStatus(int turns) { super(turns); }
+    @Override public String toString() { return "Blizzard Disorientation (" + turns + ")"; }
 }
