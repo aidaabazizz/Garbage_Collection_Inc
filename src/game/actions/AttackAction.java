850bf6a3273d37b80d9295cdf25779091d9be255
JewellGomes <cloudsontheway@gmail.com>
2026-06-04 19:58:54 +0800

    req3, refactor: implement REFLECTIVE capability and extract constants
    - Remove brittle isTargetConductive helper that relied on class-type checks.
    - Integrate MaterialCapability.REFLECTIVE for electrical arc-back logic.
    - Extract REFLECTIVE_DAMAGE and REFLECTIVE_SHOCK_DURATION as static constants.
    - Clean up execute() by delegating feedback formatting to processReflectiveFeedback.

--- src/game/actions/AttackAction.java
+++ src/game/actions/AttackAction.java
@@ -4,7 +4,7 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.Weapon;
-import game.highvoltage.MaterialCapability;
+import game.enums.MaterialCapability;
 import game.highvoltage.ShockedStatus;
 
 /**
