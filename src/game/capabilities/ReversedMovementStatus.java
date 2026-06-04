88b7f58c575e6c8d2b0f9d3e6153c883763fd7b9
chathyaattanayake <chathya@gmail.com>
2026-06-04 14:47:00 +0800

    Removed Classes which are not being used in req 4

--- src/game/capabilities/ReversedMovementStatus.java
+++
@@ -1,6 +0,0 @@
-package game.capabilities;
-
-public class ReversedMovementStatus extends TimedStatus {
-    public ReversedMovementStatus(int turns) { super(turns); }
-    @Override public String toString() { return "Spatially Inverted (" + turns + ")"; }
-}
