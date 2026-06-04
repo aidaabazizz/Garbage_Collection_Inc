fcc342730c1e3a003f7c5098bdc7bb4d3098d024
chathyaattanayake <chathya@gmail.com>
2026-06-04 14:37:23 +0800

    Add BlackHoleStatus with random teleportation and crushing damage
    - Extends DamageOverTimeStatus for duration tracking and damage
    - Deals 1 crushing damage per turn via super.tickStatus()
    - Randomly teleports affected actor to safe map location each turn
    - Finds safe warp spot (passable ground, no actor) with 20 attempts
    - Adds purple colour formatting for warp messages

--- src/game/capabilities/BlizzardDisorientationStatus.java
+++ src/game/capabilities/BlizzardDisorientationStatus.java
@@ -4,7 +4,31 @@ import edu.monash.fit2099.engine.GameEntity;
 import edu.monash.fit2099.engine.capabilities.Status;
 import edu.monash.fit2099.engine.positions.Location;
 
-public class BlizzardDisorientationStatus extends TimedStatus {
-    public BlizzardDisorientationStatus(int turns) { super(turns); }
-    @Override public String toString() { return "Blizzard Disorientation (" + turns + ")"; }
+/**
+ * A status that causes player movement to be randomized.
+ * Implements DisorientedCapability for capability pattern.
+ *
+ * @author Aida
+ */
+public class BlizzardDisorientationStatus implements Status{
+    private int remainingTurns;
+
+    public BlizzardDisorientationStatus(int duration) {
+        this.remainingTurns = duration;
+    }
+
+    @Override
+    public void tickStatus(GameEntity entity, Location location) {
+        remainingTurns--;
+    }
+
+    @Override
+    public boolean isStatusActive() {
+        return remainingTurns > 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Blizzard disorientation (" + remainingTurns + " turns left)";
+    }
 }
