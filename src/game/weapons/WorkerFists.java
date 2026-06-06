4aac26767379d92cd0527c7b514bb89c80e3cfd9
chathyaattanayake <chathya@gmail.com>
2026-06-06 12:54:29 +0800

    Refactor WorkerFists to accept damage and hitRate parameters
    - Modified constructor to accept damage and hitRate as parameters
    - Allows flexible configuration of worker fist attributes
    - Removes hardcoded values (2 damage, 100 hitRate)
    - Enables different fist variants for different contexts

--- src/game/weapons/WorkerFists.java
+++ src/game/weapons/WorkerFists.java
@@ -6,8 +6,9 @@ import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
  * A concrete implementation of the worker's bare hands.
  */
 public class WorkerFists extends IntrinsicWeapon {
-    public WorkerFists() {
+
+    public WorkerFists(int damage,int hitRate) {
         // damage: 2, verb: "punches", hitRate: 100, name: "fist"
-        super(2, "punches", 100, "fist");
+        super(damage, "punches", hitRate, "fist");
     }
 }
