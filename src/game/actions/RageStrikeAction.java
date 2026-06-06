8b3a43100b1edd9c960c96e034b128970fcf37f4
chathyaattanayake <chathya@gmail.com>
2026-06-06 12:51:46 +0800

    Enhance RageStrikeAction with heal constant and improved menu description
    - Added HEAL_AMOUNT constant (2) replacing hardcoded value
    - Added target field for menu description reference
    - Improved menuDescription() to show target name
    - Maintains life steal mechanic: heals attacker by 2 HP on successful strike

--- src/game/actions/RageStrikeAction.java
+++ src/game/actions/RageStrikeAction.java
@@ -5,9 +5,14 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.Weapon;
 
 public class RageStrikeAction extends AttackAction{
+    private final Actor target;
+    /** Amount of HP restored to the attacker on a successful rage strike. */
+    private static final int HEAL_AMOUNT = 2;
+
 
     public RageStrikeAction(Actor target, String direction, Weapon weapon) {
         super(target, direction, weapon);
+        this.target = target;
     }
 
     @Override
@@ -17,15 +22,14 @@ public class RageStrikeAction extends AttackAction{
 
         // 2. Rule 2: COMPLEX EFFECT (Life Steal)
         // If the attack was successful (you can add logic to check hit), heal the attacker.
-        int healAmount = 2;
-        actor.heal(healAmount);
+        actor.heal(HEAL_AMOUNT);
 
-        return result + String.format("\n%s absorbs the target's essence and heals %d HP!", actor, healAmount);
+        return result + String.format("\n%s absorbs the target's essence and heals %d HP!", actor, HEAL_AMOUNT);
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return actor + " unleashes a Lifesteal Rage Strike!";
+        return actor + " unleashes a Lifesteal Rage Strike on " + target + "!";
     }
 }
 
